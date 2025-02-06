ac50ecf05f32eb1dc66e83a1d8dc51a717e5f4e7
Vitor Aoki <vitoraoki17@gmail.com>
2025-02-06 15:08:09 -0300

    more features

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useRef, useState } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
 import { songsData } from "../assets/assets";
 
 export const PlayerContext = createContext();
@@ -13,11 +13,11 @@ const PlayerContextProvider = (props) => {
     const [playStatus, setPlayStatus] = useState(false);
     const [time, setTime] = useState({
         currentTime: {
-            seconds: 0,
+            seconds: '00',
             minutes: 0,
         },
         totalTime: {
-            seconds: 0,
+            seconds: '00',
             minutes: 0,
         }
     });
@@ -32,6 +32,58 @@ const PlayerContextProvider = (props) => {
         setPlayStatus(false);
     }
 
+    const playWithId = async (id) => {
+        await setTrack(songsData[id]);
+        await audioRef.current.play();
+        setPlayStatus(true);
+    }
+
+    const previous = async () => {
+        if (track.id > 0) {
+            await setTrack(songsData[track.id - 1])
+            await audioRef.current.play();
+            setPlayStatus(true);
+        }
+    }
+
+    const next = async () => {
+        if (track.id < songsData.length - 1) {
+            await setTrack(songsData[track.id + 1])
+            await audioRef.current.play();
+            setPlayStatus(true);
+        }
+    }
+
+    const seekSong = (e) => {
+        const seekWidth = seekBg.current.clientWidth;
+        const seekPosition = e.nativeEvent.offsetX;
+        const seekPercentage = seekPosition / seekWidth;
+        const seekTime = seekPercentage * audioRef.current.duration;
+        audioRef.current.currentTime = seekTime;
+    }
+
+    useEffect(() => {
+        audioRef.current.ontimeupdate = () => {
+            const currentSeconds = Math.floor(audioRef.current.currentTime % 60);
+            const currentMinutes = Math.floor(audioRef.current.currentTime / 60);
+            const totalSeconds = Math.floor(audioRef.current.duration % 60) || 0; // Evita NaN
+            const totalMinutes = Math.floor(audioRef.current.duration / 60) || 0; // Evita NaN
+
+            seekBar.current.style.width = (Math.floor(audioRef.current.currentTime / audioRef.current.duration * 100) || 0) + '%';
+
+            setTime({
+                currentTime: {
+                    seconds: String(currentSeconds).padStart(2, '0'),
+                    minutes: currentMinutes,
+                },
+                totalTime: {
+                    seconds: String(totalSeconds).padStart(2, '0'),
+                    minutes: totalMinutes,
+                }
+            });
+        };
+    }, [audioRef]);
+
     const contextValue = {
         audioRef,
         seekBar,
@@ -43,7 +95,11 @@ const PlayerContextProvider = (props) => {
         time,
         setTime,
         play,
-        pause
+        pause,
+        playWithId,
+        previous,
+        next,
+        seekSong
     }
 
     return (
